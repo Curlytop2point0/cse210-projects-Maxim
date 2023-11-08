044f5bace02e7a25d960e85928f1bc7f6f101e37
Curlytop2point0 <max.person.redskins@gmail.com>
2023-11-08 08:22:20 -0500

    Finished Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       List<int> numbers = new List<int>();
+        int input;
+
+        Console.WriteLine("Enter a series of numbers to make a list. Enter 0 to quit.");
+
+        do
+        {
+            input = Convert.ToInt32(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        } while (input != 0);
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Max: {max}");
     }
 }
